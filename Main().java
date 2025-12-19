491a84bb647265fef05bed329e717a52ae1939b3
Sidhant Saswat <sidhantsaswat19@gmail.com>
2025-12-19 20:48:32 +0530

    Integrate inventory management in CoffeeMachine
    Added inventory management and resource checking for coffee preparation.

--- Main().java
+++ Main().java
@@ -4,7 +4,9 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
 
-        // Create a Scanner object to read input
+        InventoryManager inventory = InventoryManager.loadInventory();
+        System.out.println("Current Stock -> Beans: " + inventory.coffeeBeansGram + "g | Milk: " + inventory.milkMl + "ml");
+
         Scanner keyboard = new Scanner(System.in);
 
         while (true) {
@@ -15,75 +17,62 @@ public class CoffeeMachine {
             System.out.println("3. Exit");
             System.out.print("Enter your choice (1, 2, or 3): ");
 
-            // Read user's choice
             int choice = keyboard.nextInt();
 
             switch (choice) {
                 case 1:
-                    // Initialize the name of Espresso
                     String espressoName = "Espresso";
 
-                    // Initialize the price of Espresso per serving
                     double espressoPrice = 2.50;
 
-                    // Ask the user for the type of roast and store it in espressoRoast
                     System.out.print("What Roast would you like? (light, medium, dark): ");
                     String espressoRoast = keyboard.next();
 
-                    // Ask the user for the number of shots and store it in numberOfShots
                     System.out.print("How many servings would you like? (a number please): ");
                     int numberOfShots = keyboard.nextInt();
 
-                    Espresso myEspresso = new Espresso(espressoName,espressoRoast,espressoPrice,numberOfShots);// TODO 8: Create an object of Espresso class using the parameterized constructor
-                    // TODO 8: Pass the espressoName, espressoRoast, espressoPrice, and numberOfShots as arguments in the correct order
+                    Espresso myEspresso = new Espresso(espressoName,espressoRoast,espressoPrice,numberOfShots);
+                    myEspresso.grindBeans();
+                    myEspresso.brewCoffee();
 
-                    myEspresso.grindBeans();// TODO 9: Call the grindBeans() method on the Espresso object
+                    myEspresso.printInfo();
 
-                    myEspresso.brewCoffee();// TODO 10: Call the brewCoffee() method on the Espresso object
-
-                    myEspresso.printInfo();// TODO 11: Call the printInfo() method on the Espresso object
-
-                    myEspresso.printEspressoDetails();// TODO 12: Call the printEspressoDetails method on the Espresso object
+                    myEspresso.printEspressoDetails();
 
                     break;
                 case 2:
 
-                    // Initialize the name of Latte
                     String latteName = "Latte";
 
-                    // Initialize the price of Latte
                     double lattePrice = 3.50;
 
-                    // Ask the user for the type of roast and store it in latteRoast
                     System.out.print("What Roast would you like? (light, medium, dark): ");
                     String latteRoast = keyboard.next();
 
-                    // Ask the user for the milk type and store it in milkType
                     System.out.print("What milk type would you like? (whole, skim, almond, oat): ");
                     String milkType = keyboard.next();
 
-                    // Ask the user if they want syrup or not
                     System.out.print("Would you like syrup? (yes/ no): ");
                     String syrupWanted = keyboard.next();
 
                     String syrupFlavor = "no";
-                    // if syrupWanted is yes, Ask the user for the syrup flavor and store it in syrupFlavor
                     if(syrupWanted.equals("yes")){
                         System.out.print("Which flavor would you like? (vanilla, caramel, hazelnut): ");
                         syrupFlavor = keyboard.next();
                     }
 
-                    Latte myLatte = new Latte(latteName,latteRoast,lattePrice,milkType,syrupFlavor);// TODO 20: Create an object of Latte class using the parameterized constructor
-                    // TODO 20: Pass the latteName, latteRoast, lattePrice, milkType, and syrupFlavor as arguments in the correct order
+                    if (inventory.checkResources(20, 200, 10)) {
+            
+                        Latte myLatte = new Latte(latteName, latteRoast, lattePrice, milkType, syrupFlavor);
+                        myLatte.grindBeans();
+                        myLatte.brewCoffee();
+            
+                        inventory.consume(20, 200, 10);
+                    }
 
-                    myLatte.grindBeans();// TODO 21: Call the grindBeans() method on the Latte object
+                    myLatte.printInfo();
 
-                    myLatte.brewCoffee();// TODO 22: Call the brewCoffee() method on the Latte object
-
-                    myLatte.printInfo();// TODO 23: Call the printInfo() method on the Latte object
-
-                    myLatte.printLatteDetails();// TODO 24: Call the printLatteDetails() method on the Latte object
-
+                    myLatte.printLatteDetails();
                     break;
                 case 3:
                     System.out.println("Thank you for using the Coffee Machine!");
